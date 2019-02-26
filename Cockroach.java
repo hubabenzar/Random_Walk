ee90a86b69f808813ae50242b23eb0affdad1dad
Huba Benzar <34026836+hubabenzar@users.noreply.github.com>
2019-02-26 02:17:13 +0000

    Update Cockroach.java

--- Cockroach.java
+++ Cockroach.java
@@ -1,12 +1,5 @@
-/*
-Course:			COMP 124
-Lecturer:		DR Russel Martin
-Title:			Assignment 3 - Cockroach
-Name: 			Huba Ferenc Benzar
-Student ID:		201262833
-Email:			H.Benzar@student.Liverpool.ac.uk
-Deadline: 		27/04/2018 - 1700
-*/
+//Huba Ferenc Benzar
+
 import java.util.*;
 abstract class Cockroach {
 	//Creating protected variables. String and Int
